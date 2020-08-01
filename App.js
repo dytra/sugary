d3db2c04f4827a557bd067401d84cf182348ec9d
dytra <vancielnugraha@gmail.com>
2020-08-01 18:24:40 +0700

    add set read consumptions with AsyncStorage

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { StyleSheet, View, /*Text,*/ Button, SafeAreaView } from 'react-native';
 // import { StatusBar } from 'expo-status-bar';
@@ -8,6 +8,8 @@ import { YellowBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from "./components/TabNavigator/TabNavigator";
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ModelConsumptions } from "./models";
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function App() {
   const [consumptions, setConsumptions] = useState([{
@@ -22,6 +24,28 @@ export default function App() {
   YellowBox.ignoreWarnings([
     'Non-serializable values were found in the navigation state',
   ]);
+
+
+  /* initialize sqlite db */
+  useEffect(() => {
+
+    async function getData() {
+      try {
+        const value = await AsyncStorage.getItem('consumptions');
+        if (value !== null) {
+          // value previously stored
+          console.log('init consumptions data');
+          console.log(value);
+          setConsumptions(JSON.parse(value));
+        }
+      } catch (e) {
+        // error reading value
+        console.log(e);
+      }
+    }
+    getData();
+
+  }, []);
 
   return (
 
