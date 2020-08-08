bc4edca1cea0bcbe415fc255cd9acd68da1a076c
dytra <vancielnugraha@gmail.com>
2020-08-08 23:15:33 +0700

    add amount glucose by age db

---
+++ amountGlucoseAge.js
@@ -0,0 +1,75 @@
+// source:https://kumparan.com/kumparanstyle/daftar-anjuran-konsumsi-gula-berdasarkan-kelompok-usia/full
+const data = [
+  {
+    minAge: 0,
+    maxAge: 0.58,
+    amountMin: 6,
+    amountMax: 8,
+  },
+  {
+    minAge: 0.58,
+    maxAge: 1,
+    amountMin: 6,
+    amountMax: 8
+  },
+  {
+    minAge: 1,
+    maxAge: 3,
+    amountMin: 13,
+    amountMax: 25,
+  },
+  {
+    minAge: 4,
+    maxAge: 6,
+    amountMin: 20,
+    amountMax: 39,
+  },
+  {
+    minAge: 7,
+    maxAge: 9,
+    amountMin: 23,
+    amountMax: 45,
+  },
+  {
+    minAge: 10,
+    maxAge: 12,
+    amountMin: 26,
+    amountMax: 51,
+  },
+  {
+    minAge: 13,
+    maxAge: 15,
+    amountMin: 30,
+    amountMax: 60,
+  },
+  {
+    minAge: 16,
+    maxAge: 18,
+    amountMin: 33,
+    amountMax: 65,
+  },
+  {
+    minAge: 19,
+    maxAge: 29,
+    amountMin: 32,
+    amountMax: 64,
+  },
+  {
+    minAge: 30,
+    maxAge: 49,
+    amountMin: 32,
+    amountMax: 64,
+  },
+  {
+    minAge: 50,
+    maxAge: 60,
+    amountMin: 28,
+    amountMax: 56,
+  },
+  // {
+  //   minAge:60,
+  //   maxAge:'~',
+  //   amountMin:26,
+  //   amountMax:51,
+  // }
+];
